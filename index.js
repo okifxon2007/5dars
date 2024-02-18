6754e0c015b7b7596070f33627ee8d2ef06afa31
okifxon <shokirjonovokifxon46@gmail.com>
2024-02-18 18:00:35 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,14 @@
+// string concatination stringlarni birlashtirish
+let ism = 'okifxon';
+let familiya = 'shokirjonov';
+let fullname = ism + " " + familiya;
+console.log(fullname);
+zsd
+
+
+//getting charakters bir qismini ajratib olish
+// console.log(fullname[0]);
+
+//string length
+console.log(fullname.length);
+// string methods
